a4f3cdb3993b88ab40eb567eb70c5b17b927d1e5
Ant L <anthonyl1993@gmail.com>
2015-09-08 12:31:55 -0400

    first commit for antl.me

---
+++ js/custom.js
@@ -0,0 +1,74 @@
+/* jQuery to collapse the navbar on scroll */
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+/* Sticky Navbar */
+$(document).ready(function(){
+    $(".navbar").sticky({topSpacing:0});
+});
+
+/* Auto Close Responsive Navbar on Click */
+function close_toggle() {
+if ($(window).width() <= 768) {
+  $('.navbar-collapse a').on('click', function(){
+      $('.navbar-collapse').collapse('hide');
+  });
+}
+else {
+ $('.navbar .navbar-default a').off('click');
+}
+}
+close_toggle();
+
+$(window).resize(close_toggle); 
+
+/* jQuery page scrolling feature */
+$(function() {
+    $('.navbar a,.btn').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 50
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+     /* WOW */  
+   wow = new WOW(
+      {
+        animateClass: 'animated',
+        offset:       100,
+        mobile:       true
+      }
+    );
+    wow.init();
+
+/* Parallax */
+$(function(){
+			$.stellar({
+				horizontalScrolling: false
+			});
+		});
+
+/* niceScroll */
+jQuery("html").niceScroll({
+    scrollspeed: 50,
+    mousescrollstep: 38,
+    cursorwidth: 7,
+    cursorborder: 0,
+    cursorcolor: '#35bdf6',
+    autohidemode: false,
+    zindex: 9999999,
+    horizrailenabled: false,
+    cursorborderradius: 0
+});
+
+
+
+
+
+
